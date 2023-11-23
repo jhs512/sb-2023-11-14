b95d807b8d1069080130011dd5800445d902b6df
jhs512 <jangka512@gmail.com>
2023-11-23 14:26:03 +0900

    step 38

--- src/test/java/com/ll/sb20231114/domain/home/home/controller/AdmHomeControllerTest.java
+++ src/test/java/com/ll/sb20231114/domain/home/home/controller/AdmHomeControllerTest.java
@@ -51,4 +51,21 @@ public class AdmHomeControllerTest {
                 .andExpect(handler().methodName("showMain"))
                 .andExpect(view().name("home/home/adm/main"));
     }
+
+    @Test
+    @DisplayName("/adm/home/about")
+    @WithUserDetails("admin")
+    void t3() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/adm/home/about"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(AdmHomeController.class))
+                .andExpect(handler().methodName("showAbout"))
+                .andExpect(view().name("home/home/adm/about"));
+    }
 }
