11afcb617db3ebefd7915bfd9a529bb6d687ebae
jhs512 <jangka512@gmail.com>
2023-11-23 11:49:05 +0900

    step 22

--- src/test/java/com/ll/sb20231114/domain/article/article/controller/ArticleControllerTest.java
+++ src/test/java/com/ll/sb20231114/domain/article/article/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.ll.sb20231114.domain.article.article.controller;
 
+import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 public class ArticleControllerTest {
     @Autowired
     private MockMvc mvc;
+    @Autowired
+    private ArticleService articleService;
 
     // GET /article/list
     @Test
@@ -27,8 +32,25 @@ public class ArticleControllerTest {
         ResultActions resultActions = mvc
                 .perform(get("/article/list"))
                 .andDo(print());
-    }
 
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(ArticleController.class))
+                .andExpect(handler().methodName("showList"))
+                .andExpect(content().string(containsString("""
+                        게시글 목록
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        3번 : 제목3
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        2번 : 제목2
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        1번 : 제목1
+                        """.stripIndent().trim())));
+    }
 
     // GET /article/detail/{id}
     // GET /article/write
