4d1d29d8516788aab6cd243e37ece7b5387da634
jhs512 <jangka512@gmail.com>
2023-11-23 13:49:09 +0900

    step 32

--- src/test/java/com/ll/sb20231114/domain/member/member/controller/MemberControllerTest.java
+++ src/test/java/com/ll/sb20231114/domain/member/member/controller/MemberControllerTest.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,42 +27,26 @@ public class MemberControllerTest {
     // GET /member/login
     @Test
     @DisplayName("로그인 페이지를 보여준다")
-    @WithAnonymousUser
     void showLoginPage() throws Exception {
-        mvc.perform(get("/member/login"))
-                .andDo(print())
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/login"))
+                .andDo(print());
+
+        // THEN
+        resultActions
                 .andExpect(status().isOk())
                 .andExpect(view().name("member/member/login"))
                 .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showLogin"));
-    }
-
-    // GET /member/join
-    @Test
-    @DisplayName("회원가입 페이지를 보여준다")
-    @WithAnonymousUser
-    void showJoinPage() throws Exception {
-        mvc.perform(get("/member/join"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("member/member/join"))
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showJoin"));
-    }
-
-    // POST /member/join
-    @Test
-    @DisplayName("회원가입 처리")
-    @WithAnonymousUser
-    void joinMember() throws Exception {
-        mvc.perform(post("/member/join")
-                        .with(csrf())
-                        .param("username", "newuser")
-                        .param("password", "newpassword"))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/member/login?msg=**"))
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"));
+                .andExpect(handler().methodName("showLogin"))
+                .andExpect(content().string(containsString("""
+                        로그인
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())));
     }
 }
