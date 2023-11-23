fad509ebf306647998da1da129f4616c10661015
jhs512 <jangka512@gmail.com>
2023-11-23 13:28:28 +0900

    step 29

--- src/test/java/com/ll/sb20231114/domain/article/article/controller/ArticleControllerTest.java
+++ src/test/java/com/ll/sb20231114/domain/article/article/controller/ArticleControllerTest.java
@@ -16,8 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -63,9 +62,9 @@ public class ArticleControllerTest {
                 .perform(get("/article/detail/1"))
                 .andDo(print());
 
+        // THEN
         Article article = articleService.findById(1L).get();
 
-        // THEN
         resultActions
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(ArticleController.class))
@@ -157,9 +156,9 @@ public class ArticleControllerTest {
                 .perform(get("/article/modify/1"))
                 .andDo(print());
 
+        // THEN
         Article article = articleService.findById(1L).get();
 
-        // THEN
         resultActions
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(ArticleController.class))
@@ -177,6 +176,33 @@ public class ArticleControllerTest {
                         >%s</textarea>
                         """.formatted(article.getBody()).stripIndent().trim())))
         ;
+    }
+
+    @Test
+    @DisplayName("게시물 수정폼 처리")
+    @WithUserDetails("admin")
+    void t7() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(
+                        put("/article/modify/1")
+                                .with(csrf())
+                                .param("title", "제목 new")
+                                .param("body", "내용 new")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().handlerType(ArticleController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(redirectedUrlPattern("/article/list?msg=**"));
+
+        Article article = articleService.findById(1L).get();
+
+        assertThat(article.getTitle()).isEqualTo("제목 new");
+        assertThat(article.getBody()).isEqualTo("내용 new");
     }
 
     // PUT /article/modify/{id}
