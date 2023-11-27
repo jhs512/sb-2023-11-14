f5144c7f383953064678607a90eef9514e996281
jhs512 <jangka512@gmail.com>
2023-11-27 11:35:59 +0900

    step 9

--- src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
+++ src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -11,5 +13,6 @@ public class Attr {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    private LocalDateTime createDate;
     private String name;
 }
