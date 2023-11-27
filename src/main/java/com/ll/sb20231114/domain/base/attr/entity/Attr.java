f9831cf79487c31312840225262968044e476bd8
jhs512 <jangka512@gmail.com>
2023-11-27 11:31:46 +0900

    step 7

---
+++ src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
@@ -0,0 +1,14 @@
+package com.ll.sb20231114.domain.base.attr.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Attr {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+}
